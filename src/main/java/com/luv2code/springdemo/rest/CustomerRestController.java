3cbd8c1e5538af149247e8647c876a61c839ca10
Chaitanya-Sarma-hub <iamsarma030@gmail.com>
2022-11-10 11:37:36 +0530

    added update feature

--- src/main/java/com/luv2code/springdemo/rest/CustomerRestController.java
+++ src/main/java/com/luv2code/springdemo/rest/CustomerRestController.java
@@ -5,6 +5,9 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -27,9 +30,22 @@ public class CustomerRestController {
 	@GetMapping("/customers/{customerId}")
 	public Customer getCustomer(@PathVariable int customerId) {
 		Customer customer = customerService.getCustomer(customerId);
-//		if (customer == null) {
-//			throw new CustomerNotFoundException("Customer id not found: " + customerId);
-//		}
+		if (customer == null) {
+			throw new CustomerNotFoundException("Customer id not found: " + customerId);
+		}
+		return customer;
+	}
+
+	@PostMapping("/customers")
+	public Customer addCustomer(@RequestBody Customer customer) {
+		customer.setId(0);
+		customerService.saveCustomer(customer);
+		return customer;
+	}
+
+	@PutMapping("/customers")
+	public Customer updateCustomer(@RequestBody Customer customer) {
+		customerService.saveCustomer(customer);
 		return customer;
 	}
 }
